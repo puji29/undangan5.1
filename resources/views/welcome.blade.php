82052aa2255e16675cb828c1b01b8b02d33bbae2
puji29 <66867777+puji29@users.noreply.github.com>
2024-07-27 10:55:18 +0000

    [ADD] init awal

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <!-- welcom -->
+    <div
+      class="welcome h-screen bg-[url('/image/bg_welcome.jpg')] bg-cover content-center items-center text-center"
+    >
+      <div class="w-[50%] rounded-lg shadow-lg backdrop-blur-sm mx-auto">
+        <h2>you are invited to our wedding</h2>
+        <h1>Putra & Putri</h1>
+        <p>Rabu, 29 November 2024</p>
+        <div class="rounded-lg shadow-lg mx-auto py-2">
+          <p>Kepada Yth. Bpk/Ibu/Saudara/i</p>
+          <p>Romeo</p>
+          <p>di tempat</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
